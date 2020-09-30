ed2db7ebd2cbcf8417ee7d8b6c5a255bd20afdbe
Ekaterina Pokras <eapeip@mail.ru>
2020-10-01 01:55:56 +0500

    feat: implement 3 functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined ) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  }
+    
+  let minimum = array[0];
+  
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < minimum) {
+      minimum = array[i];
+    }
+  }
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array == undefined ) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  }
+  let maximum = array[0];
+  
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] > maximum) {
+      maximum = array[i];
+    }
+  }
+  return maximum;
+ }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined ) {
+    return 0;
+  } else if (array.length === 0) {
+    return 0;
+  }
+  let res = 0;
+    for (var i = 0; i < array.length; i++) {
+      res += array[i];
+    }
+  res /= array.length;
+  return res;
 }
